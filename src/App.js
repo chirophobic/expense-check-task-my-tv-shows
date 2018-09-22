b660086f3e4cbc0422e2f56a715bb3abeeb0b62a
bohdan <bohdan.gibert@gmail.com>
2018-09-22 18:25:07 +1000

    Removed unused files and styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
 import './App.css';
+import routes from './routes';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    static renderNavLink (route) {
+        return <NavLink exact={true} activeClassName="foobar" to={route.path}>{route.title}</NavLink>;
+    }
+
+    static renderRoute (route) {
+        return <Route exact={!!route.exact} path={route.path} component={route.component}/>;
+    }
+
+    render () {
+        return (
+            <Router>
+                <div className="App">
+                    <header className="header">
+                        <h1 className="App-title">Welcome to React</h1>
+                        {routes.map(App.renderNavLink)}
+                    </header>
+                    <main className="main-content">
+                        {routes.map(App.renderRoute)}
+                    </main>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
