a484b055a39bbdf774ab748c31abae8774d70a8e
bohdan <bohdan.gibert@gmail.com>
2018-09-23 22:39:48 +1000

    Refactored and linked up search on the home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,81 @@
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
+import {faPlus, faThumbsDown, faThumbsUp, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+import debounce from 'debounce';
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
-import {listMovies} from '../../data/the-movie-db';
-import MovieList from '../../shared-components/MovieList';
+import React, {Component, Fragment} from 'react';
+import config from '../../config';
+
+function Movie ({movie, addToWatchList, removeFromWatchList, addToFavourites, removeFromFavourites}) {
+    const addOrRemoveWatchList = movie.is_in_watch_list ? removeFromWatchList : addToWatchList;
+    const addOrRemoveFromFavourites = movie.is_in_favourites ? removeFromFavourites : addToFavourites;
+    return (
+        <li className="movie">
+            <img className="movie__cover"
+                 src={`${config.imageBaseUrl}w300${movie.poster_path}`}
+                 alt={movie.title}/>
+            <div className="movie__title">{movie.name}</div>
+            <div className="movie__rating">{movie.vote_average}</div>
+            <div className="movie__actions">
+                <div className="movie__actions__action"
+                     title="Add to Watch List"
+                     onClick={() => addOrRemoveWatchList(movie)}>
+                    <FontAwesomeIcon icon={movie.is_in_watch_list ? faTrashAlt : faPlus}/>
+                </div>
+                <div className="movie__actions__action"
+                     title="Add to Favourites"
+                     onClick={() => addOrRemoveFromFavourites(movie)}>
+                    <FontAwesomeIcon icon={movie.is_in_favourites ? faThumbsDown : faThumbsUp}/>
+                </div>
+                <div className="movie__actions__action" title="Watch Trailer">
+                    <FontAwesomeIcon icon={faYoutube}/>
+                </div>
+            </div>
+        </li>
+    );
+}
 
 class Home extends Component {
     constructor (props) {
         super(props);
-        this.state = {movies: []};
-        this.loadMovies();
+        this.onSearch = debounce(this.onSearch, 300);
+        this.state = {movies: [], searchTerm: '', page: 1};
+        this.baseQueryParams = {
+            'api_key': config.apiToken,
+        };
+        this.listMovies();
     }
 
-    loadMovies () {
-        listMovies().then(movies => this.setState({movies: movies}));
+    listMovies () {
+        const params = {params: {...this.baseQueryParams, page: this.state.page}};
+        axios.get(`${config.apiBaseUrl}/discover/tv`, params)
+             .then(response => this.handleApiSuccess(response));
+    }
+
+    searchMovies () {
+        const options = {params: {...this.baseQueryParams, query: this.state.searchTerm, page: this.state.page}};
+        axios.get(`${config.apiBaseUrl}/search/tv`, options)
+             .then(response => this.handleApiSuccess(response));
+    }
+
+    loadMore () {
+        this.setState({page: this.state.page + 1}, () => {
+            if (this.state.searchTerm) {
+                this.searchMovies();
+            } else {
+                this.listMovies();
+            }
+        });
+    }
+
+    onSearch (searchTerm) {
+        this.setState({searchTerm, movies: [], page: 1}, this.loadMore);
+    }
+
+    handleApiSuccess (response) {
+        console.log(response.data.results);
+        this.setState({movies: [...this.state.movies, ...response.data.results]});
     }
 
     mapMovieToUsableType (movie) {
@@ -25,11 +89,25 @@ class Home extends Component {
     render () {
         const movies = this.state.movies.map(movie => this.mapMovieToUsableType(movie));
         return (
-            <MovieList movies={movies}
-                       addToWatchList={this.props.watchList.add}
-                       removeFromWatchList={this.props.watchList.remove}
-                       addToFavourites={this.props.favourites.add}
-                       removeFromFavourites={this.props.favourites.remove}/>
+            <Fragment>
+                <div className="search">
+                    <input onChange={e => this.onSearch(e.target.value)}
+                           className="search__input"
+                           placeholder="Search"
+                           type="search"/>
+                </div>
+                <ul className="movies-list">
+                    {movies.map(movie => <Movie key={movie.id}
+                                                movie={movie}
+                                                addToWatchList={this.props.watchList.add}
+                                                removeFromWatchList={this.props.watchList.remove}
+                                                addToFavourites={this.props.favourites.add}
+                                                removeFromFavourites={this.props.favourites.remove}/>)}
+                </ul>
+                <div className="load-more">
+                    <div onClick={() => this.loadMore()} className="load-more__button">Load More</div>
+                </div>
+            </Fragment>
         );
     }
 }
