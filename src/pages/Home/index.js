8caab186ad06e0eb0e8f76d6bb7e31f288a3caf8
bohdan <bohdan.gibert@gmail.com>
2018-09-23 18:22:07 +1000

    Linked up the home page to the movies list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,71 @@
+import PropTypes from 'prop-types';
 import React, {Component} from 'react';
+import {listMovies} from '../../data/the-movie-db';
+import MovieList from '../../shared-components/MovieList';
 
 class Home extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {movies: []};
+        this.loadMovies();
+    }
+
+    loadMovies () {
+        listMovies().then(movies => {
+            this.setState({movies: this.mapMoviesToUsableType(movies)});
+        });
+    }
+
+    addToWatchList (movie) {
+        this.props.watchList.add(movie);
+        this.setState({movies: this.mapMoviesToUsableType(this.state.movies)});
+    }
+
+    addToFavourites (movie) {
+        this.props.favourites.remove(movie);
+        this.setState({movies: this.mapMoviesToUsableType(this.state.movies)});
+    }
+
+    removeFromWatchList (movie) {
+        this.props.watchList.add(movie);
+        this.setState({movies: this.mapMoviesToUsableType(this.state.movies)});
+    }
+
+    removeFromFavourites (movie) {
+        this.props.favourites.remove(movie);
+        this.setState({movies: this.mapMoviesToUsableType(this.state.movies)});
+    }
+
+    mapMoviesToUsableType (movies) {
+        return movies.map(movie => {
+            return {
+                ...movie,
+                is_in_watch_list: this.props.watchList.contains(movie),
+                is_in_favourites: this.props.favourites.contains(movie),
+            };
+        });
+    }
+
     render () {
         return (
-            <div>Hello! This is the home page</div>
-        )
+            <MovieList movies={this.state.movies}
+                       addToWatchList={this.addToWatchList.bind(this)}
+                       removeFromWatchList={this.removeFromWatchList.bind(this)}
+                       addToFavourites={this.addToFavourites.bind(this)}
+                       removeFromFavourites={this.removeFromFavourites.bind(this)}/>
+        );
     }
 }
+
+const shape = {
+    add: PropTypes.func,
+    remove: PropTypes.func,
+    contains: PropTypes.func,
+};
+
+Home.propTypes = {
+    watchList: PropTypes.shape(shape),
+    favourites: PropTypes.shape(shape),
+};
 
 export default Home;
