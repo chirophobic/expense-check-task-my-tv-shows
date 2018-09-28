dd51528785fd171bc81eff9f41f78931ecff1a11
Bohdan Gibert <bohdan.gibert@gmail.com>
2018-09-28 18:08:05 +1000

    Added an image with the similar show

--- src/pages/FavouritesAndWatchList/SimilarShows.js
+++ src/pages/FavouritesAndWatchList/SimilarShows.js
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import config from '../../config';
 import './SimilarShows.css';
+import PosterImage from '../../shared-components/PosterImage';
+
+function SimilarShow ({show}) {
+    return (
+        <li className="similar-shows__list__show">
+            <PosterImage alt={show.name} path={show.poster_path} width={200} height={250}/>
+            {show.name}
+        </li>
+    );
+}
 
 class SimilarShows extends Component {
     constructor (props) {
@@ -13,17 +23,16 @@ class SimilarShows extends Component {
     componentDidMount () {
         const params = {params: {'api_key': config.apiToken}};
         axios.get(`${config.apiBaseUrl}/tv/${this.props.show.id}/similar`, params)
-             .then(response => this.setState({similar: response.data.results}));
+            .then(response => this.setState({similar: response.data.results}));
     }
 
     render () {
         return (
             <div className="similar-shows">
                 <div className="similar-shows__header">Similar Shows:</div>
-                <ul>
-                    {this.state.similar.map(show => <li key={show.id}>{show.name}</li>)}
+                <ul className="similar-shows__list">
+                    {this.state.similar.map(show => <SimilarShow key={show.id} show={show}/>)}
                 </ul>
-
             </div>
         );
     }
